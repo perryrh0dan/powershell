14ce7eca10c0fd50dc92ead820fb4b1e673080dc
thomas <thomas@tpoe.dev>
2023-10-30 17:42:58 +0100

    feat(zsh): mount history to dev container

--- functions/dev.ps1
+++ functions/dev.ps1
@@ -19,6 +19,11 @@ if ($VolumeOrDirectory) {
 		$mountType = 'volume'
 	}
 
+	$history = "dev-history"
+	if ($mountType -eq "volume") {
+		$history = $directoryOrVolume + '-history'
+	}
+
 	$ports = ""
 	if ($Port) {
 		$prefix = "-p"
@@ -26,7 +31,7 @@ if ($VolumeOrDirectory) {
 		$ports = $ports + $prefix + $mapping + " " 
 	}
 
-	Invoke-Expression "docker run ${ports} --rm --mount type=${mountType},src=${directoryOrVolume},target=/root/workspace --mount type=bind,src=$SSH_DIRECTORY,target=/root/.ssh -v //var/run/docker.sock://var/run/docker.sock -it $DOCKER_DEV_ENV" 
+	Invoke-Expression "docker run ${ports} --rm --mount type=${mountType},src=${directoryOrVolume},target=/root/workspace --mount type=bind,src=$SSH_DIRECTORY,target=/root/.ssh  --mount type=volume,src=$history,target=/root/.history --mount type=bind,src=//var/run/docker.sock,target=//var/run/docker.sock -it $DOCKER_DEV_ENV" 
 } else {
 	$remotes = $REMOTE_DEV_ENV.split(",")
 	foreach ($remote in $remotes) {
