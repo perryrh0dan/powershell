23794d9e003a3366d5dba547d089c4023c2a15bc
thomas <thomas@tpoe.dev>
2024-03-02 17:53:43 +0100

    feat(dev): split code and make gpg and ssh dir optional

--- functions/dev.ps1
+++ functions/dev.ps1
@@ -56,7 +56,21 @@ if ($VolumeOrDirectory) {
         $Host.UI.RawUI.WindowTitle = "dev ${absPath}"
     }
 
-	Invoke-Expression "docker run ${ports} --rm --mount type=${mountType},src=${directoryOrVolume},target=/root/workspace --mount type=bind,src=$SSH_DIRECTORY,target=/root/.ssh --mount type=bind,src=$GPG_DIRECTORY,target=/root/.gnupg --mount type=volume,src=$history,target=/root/.history --mount type=volume,src=$zoxide,target=/root/.local/share/zoxide --mount type=bind,src=//var/run/docker.sock,target=//var/run/docker.sock -it $DOCKER_DEV_ENV" 
+    $sshMount = ""
+    if ($SSH_DIRECTORY) {
+        $sshMount = "--mount type=bind,src=$SSH_DIRECTORY,target=/root/.ssh" 
+    }
+
+    $gpgMount = ""
+    if ($GPG_DIRECTORY) {
+        $gpgMount = "--mount type=bind,src=$GPG_DIRECTORY/pubring.kbx,target=/root/.gnupg/pubring.kbx --mount type=bind,src=$GPG_DIRECTORY/trustdb.gpg,target=/root/.gnupg/trustdb.gpg --mount type=bind,src=$GPG_DIRECTORY/private-keys-v1.d,target=/root/.gnupg/private-keys-v1.d"
+    } 
+
+    $dockerMount = "--mount type=bind,src=//var/run/docker.sock,target=//var/run/docker.sock"
+    $historyMound = "--mount type=volume,src=$history,target=/root/.history"
+    $xoxideMount = "--mount type=volume,src=$zoxide,target=/root/.local/share/zoxide"
+
+	Invoke-Expression "docker run ${ports} --rm --mount type=${mountType},src=${directoryOrVolume},target=/root/workspace $sshMount $gpgMount $historyMount $zoxideMount $dockerMount -it $DOCKER_DEV_ENV" 
 
     # Undo title change
     $Host.UI.RawUI.WindowTitle = "Windows PowerShell"
