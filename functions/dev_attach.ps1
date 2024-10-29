e1e6639619d76126644d9e28b9ebe5d367110439
thomas <thomas@tpoe.dev>
2024-10-29 17:09:54 +0100

    feat(dev): add memory limit and rename window title on attach

--- functions/dev_attach.ps1
+++ functions/dev_attach.ps1
@@ -6,7 +6,13 @@ docker ps -a -q --filter id=$ContainerId
 $exitCode = $LASTEXITCODE
 
 if ($exitCode -eq 0) {
+    $Host.UI.RawUI.WindowTitle = "dev ${ContainerId}"
+
     docker exec -it $ContainerId /root/start.sh
+
+    # Undo title change
+    $Host.UI.RawUI.WindowTitle = "Windows PowerShell"
+
 } else {
     Write-Host "Could not find a container with id: ${ContainerId}"
 }
